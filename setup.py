f31d095e5f1a2dbe4828fa220ff4dea7e90e43d1
Zanaca <carlos.rios@hotelurbano.com.br>
2016-09-01 19:22:30 -0300

    upating package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='rezizer',
     packages=['rezizer'],
-    version='0.1.0',
+    version='0.1.2',
     description='Rezizer URL generator - Building optmized image URLs',
     author='Zanaca',
     author_email='zanaca@gmail.com',
