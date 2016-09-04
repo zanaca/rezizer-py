74e85e92dad2b7f0aa91c1ca3a6f3a07f4922d99
Carlos Rios <carlos@zanaca.com>
2016-09-04 16:56:04 -0300

    updating license

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='rezizer',
     packages=['rezizer'],
-    version='0.2.0',
+    version='0.2.1',
     description='Rezizer URL generator - Building optmized image URLs',
     author='Zanaca',
     author_email='zanaca@gmail.com',
     url='https://github.com/zanaca/rezizer-py',
-    download_url='https://github.com/zanaca/rezizer/tarball/0.2.0',
+    download_url='https://github.com/zanaca/rezizer/tarball/0.2.1',
     keywords=['resize', 'crop', 'optmize', 'thumbor', 'imageurl', 'rezizer'],
     classifiers=[],
 )
